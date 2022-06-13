3b3cc18b645628a3e26301cc8ae05478671f9722
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-06-12 20:35:30 -0700

    added code for questions, added assests file with background

--- questions.js
+++ questions.js
@@ -1,12 +1,12 @@
 let questions = [
-    {text : "Only one color, but not one size, stuck at the bottom, yet easily flies. Present in sun, but not in rain, doing no harm, and feeling no pain. What am I?", 
-    correctAnswer : "SHADOW"},
-    {text: "Who is that with a neck and no head, two arms and no hands? What am I?", 
-    correctAnswer : "SHIRT"},
-    {text : "It can't be seen, can't be felt, can't be heard, and can't be smelt. It lies behind stars and under hills, and empty holes it fills. It comes first and follows after, ends life, and kills laughter. What is it?",
-    correctAnswer: "THE DARK"},
-    {text : "What is the only word in the English language that has 3 double letters back to back?", 
-    correctAnswer: "BOOKKEEPER"},
-    {text: "What belongs to you, but other people use more that you?",
-    correctAnswer: "YOUR NAME"}
+    {text1 : "Only one color, but not one size, stuck at the bottom, yet easily flies. Present in sun, but not in rain, doing no harm, and feeling no pain. What am I?", 
+    correctAnswer1 : "SHADOW"},
+    {text2: "Who is that with a neck and no head, two arms and no hands? What am I?", 
+    correctAnswer2 : "SHIRT"},
+    {text3 : "It can't be seen, can't be felt, can't be heard, and can't be smelt. It lies behind stars and under hills, and empty holes it fills. It comes first and follows after, ends life, and kills laughter. What is it?",
+    correctAnswer3: "THE DARK"},
+    {text4 : "What is the only word in the English language that has 3 double letters back to back?", 
+    correctAnswer4: "BOOKKEEPER"},
+    {text5: "What belongs to you, but other people use more that you?",
+    correctAnswer5: "YOUR NAME"}
 ]
