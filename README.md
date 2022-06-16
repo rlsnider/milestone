458f94ff23d9abb4650c76c0a2a227f6f058410e
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-06-15 18:45:54 -0700

    latest commit

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # milestone description
 # riddle game
-## Five questions
-## Meet someone in your travels who wont let you cross bridge until you answer 3 out of 5 questions correctly.
-## Answer Correlcly, get one point, incorrectly, stranger says he'll give you another chance, as long as you havent missed 3 out of 5 questions.
-## Once you answer 3 or of 5 correctly, he allows you to cross the bridge with the warning that the next challenge will be harder.
-# If you lose, he dances around and laughs at you
+-- Create 5 questions for user to answer in order to cross the "bridge".
+-- Answer Correlcly, get one point, incorrectly, stranger says he'll give you another chance, as long as you havent missed 3 out of 5 questions.
+-- Once you answer 3 out of 5 correctly, he allows you to cross the bridge with the warning that the next challenge will be harder.
+--Lose, and he dances around and laughs at you.
 
 
--- index.js
+++ index.js
@@ -1,19 +1,49 @@
+//set arrays for functions to read
 
-//set counter to strt
-let userScore =1;
+let answers=["SHADOW"];
+let userAnswer=[];
 //set all buttons invisibile except for start button.
 document.getElementById("btn2").style.display ="none";
 document.getElementById('btn3').style.display ="none";
-document.getElementById('btn4').style.display ="none";
+
+const addUserAnswer=(ev)=>{
+    ev.preventDefault();//to stop form from submitting
+  
+}
+
+
+
 
 
 function statement1(){
+    
     //hide starting button show button 2
-    document.getElementById('statrBtn').style.display ="none";
-    document.getElementById('btn2').style.display =" ";
+    document.getElementById('startBtn').style.display ="none";
+    document.getElementById('btn2').style.display = "";
     //hide first dialog and show first question
     document.getElementById('startup').style.display="none";
-    document.getElementById('statrup').innerHTML = "Only one color, but not one size, stuck at the bottom, yet easily flies. Present in sun, but not in rain, doing no harm and feeling no pain. What am I?"
+    document.getElementById('dialog1').innerHTML = "Only one color, but not one size,<br> stuck at the bottom, yet easily flies.<br>Present in sun, but not in rain,<br> doing no harm and feeling no pain.<br> What am I?";
 }
 //waits for onclick from button, then runs statement 2
-function statement2()
+//turn off display of btn2, open up btn3, turn off dialog1, 
+//creates new object from "userAnswer" add(push)answer into array, set Answer to string with JSON. 
+//compare userinput to 0 in answers array, if match, turn on dialog2, turn off button 2, open up button 3.
+function statement2(){
+    document.getElementById("btn2").style.display ="none";
+    document.getElementById('btn3').style.display="";
+   
+    document.getElementById("dialog1").style.display= "none";
+    let answer ={users: document.getElementById("users").value,
+    }
+    userAnswer.push(answer);
+
+    localStorage.setItem("ansr", JSON.stringify(userAnswer));
+    }
+    
+    let userGuess=userAnswer[0]
+    let correctAnswer=answers[0]
+    if(userGuess===correctAnswer){
+        document.getElementById('dialog1').innerHTML ="I guess I'll have to give you that one, but you won't be able to answer the next one!";
+        document.getElementById('btn2').style.display ="none";
+        document.getElementById("btn3").style.display=" ";
+    }
